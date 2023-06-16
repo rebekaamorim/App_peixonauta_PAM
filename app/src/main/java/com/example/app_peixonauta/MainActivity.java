a7b6e896cb2d9c9a8256c1208bee40ecf406bc2e
Vitória Campelo <127426462+Vhibis6@users.noreply.github.com>
2023-06-16 09:28:57 -0300

    Modificações na segunda tela e continuação da programação.

--- app/src/main/java/com/example/app_peixonauta/MainActivity.java
+++ app/src/main/java/com/example/app_peixonauta/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn
 
     }
     // Definindo botãos
