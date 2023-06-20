c7f8268c78e049d9f0ce3240824c8ce7f657aeb8
Rebeka Lima <127617481+rebekaamorim@users.noreply.github.com>
2023-06-20 10:03:07 -0300

    Btn

--- app/src/main/java/com/example/app_peixonauta/Lista_de_historias.java
+++ app/src/main/java/com/example/app_peixonauta/Lista_de_historias.java
@@ -10,6 +10,19 @@ public class Lista_de_historias extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_de_historias);
-    }
-}
 
+avancar = (Button)findViewById(R.id.)
+
+avancar.setOnClickListener(New View.OnClickListener(){
+
+           @override
+           public void OnClick (View v)
+           Intent in = new Intent (Lista_de_historias.this, Tela_historia.class);
+
+           startActivity (in);
+         });
+    }
+
+   Button avancar
+
+}
